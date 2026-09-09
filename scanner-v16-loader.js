4a1b81158e5903d07b1131a0d966e31d61aca9f7
Bennyescaped <benny.escaped@googlemail.com>
2026-09-09 18:51:56 +0200

    Scanner V16: load market intelligence module

--- scanner-v16-loader.js
+++ scanner-v16-loader.js
@@ -1,7 +1,7 @@
 (()=>{
   'use strict';
   const add=(src,onload)=>{const s=document.createElement('script');s.src=src;s.defer=true;if(onload)s.onload=onload;document.body.appendChild(s)};
-  const loadV16=()=>add('scanner-v16-core.js?v=16.0.0',()=>add('scanner-v16-binder.js?v=16.0.0',()=>add('scanner-v16-ui.js?v=16.0.0')));
+  const loadV16=()=>add('scanner-v16-core.js?v=16.0.0',()=>add('scanner-v16-binder.js?v=16.0.0',()=>add('scanner-v16-market.js?v=16.0.0',()=>add('scanner-v16-ui.js?v=16.0.0'))));
   if(window.DV_SCAN_V15)loadV16();
   else add('scanner-v15-loader.js?v=15.8',loadV16);
 })();
