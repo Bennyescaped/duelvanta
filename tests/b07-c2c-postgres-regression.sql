de8fcb12e790a09f0e2bf10f3d49009dab49e294
Bennyescaped <benny.escaped@googlemail.com>
2026-09-16 13:19:36 +0200

    test(B07): regress real staging C2C completion schema

--- tests/b07-c2c-postgres-regression.sql
+++ tests/b07-c2c-postgres-regression.sql
@@ -1,4 +1,7 @@
 \set ON_ERROR_STOP on
+\ir b07-c2c-staging-schema-overlay.sql
+\ir ../database/b07-l07-01-c2c-swap-v1-completion-compat.sql
 \ir b07-c2c-postgres-regression-base.sql
 \ir ../database/b07-l07-01-c2c-swap-v1-pickup.sql
+\ir ../database/b07-l07-01-c2c-swap-v1-pickup-completion-compat.sql
 \ir b07-c2c-pickup-postgres-regression.sql
