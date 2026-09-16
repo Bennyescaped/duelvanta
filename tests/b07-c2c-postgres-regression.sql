5092e272984bd6804be09f855df673aad0e2235a
Bennyescaped <benny.escaped@googlemail.com>
2026-09-16 20:07:54 +0200

    test(B07): include pickup conversation list regression

--- tests/b07-c2c-postgres-regression.sql
+++ tests/b07-c2c-postgres-regression.sql
@@ -7,4 +7,5 @@
 \ir b07-c2c-pickup-postgres-regression.sql
 \ir b07-pickup-messages-bootstrap.sql
 \ir ../database/b07-l07-01-pickup-messages-v1.sql
+\ir ../database/b07-l07-01-pickup-messages-v1-list.sql
 \ir b07-pickup-messages-postgres-regression.sql
